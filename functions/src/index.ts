1125a420445e93c2302359ee314366cd0886aa54
andremusiol <andremusiol@users.noreply.github.com>
2020-05-11 17:11:13 +0200

    added sample cloud functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,19 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
+
+// Make them public like this
+// https://cloud.google.com/functions/docs/securing/managing-access-iam#allowing_unauthenticated_function_invocation
+
+export const helloWorld = functions.region('europe-west3').https.onRequest((request, response) => {
+  response.send('Hello from Firebase!');
+});
+
+export const addMessage = functions.region('europe-west3').https.onRequest(async (request, response) => {
+  admin.initializeApp();
+  const original = request.query.text;
+  const snapshot = await admin.database().ref('/messages').push({ original: original });
+  response.redirect(303, snapshot.ref.toString());
+});
