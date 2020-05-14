e1a2da32247992c3c7eceee4bbaf50cc80e02bf4
andremusiol <andremusiol@users.noreply.github.com>
2020-05-14 15:46:06 +0200

    fixed root nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,77 +1,77 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 
 const routes: Routes = [
   {
-    path: '',
-    redirectTo: 'posteingang',
-    pathMatch: 'full',
+    path: "",
+    redirectTo: "posteingang",
+    pathMatch: "full",
   },
   {
-    path: 'folder/:id',
+    path: "folder/:id",
     loadChildren: () =>
-      import('./pages/folder/folder.module').then((m) => m.FolderPageModule),
+      import("./pages/folder/folder.module").then((m) => m.FolderPageModule),
   },
   {
-    path: 'anwesenheit',
+    path: "anwesenheit",
     loadChildren: () =>
-      import('./pages/anwesenheit/anwesenheit.module').then(
+      import("./pages/anwesenheit/anwesenheit.module").then(
         (m) => m.AnwesenheitPageModule
       ),
   },
   {
-    path: 'klassen',
+    path: "klassen",
     loadChildren: () =>
-      import('./pages/klassensteckbrief/klassensteckbrief.module').then(
+      import("./pages/klassensteckbrief/klassensteckbrief.module").then(
         (m) => m.KlassensteckbriefPageModule
       ),
   },
   {
-    path: 'benutzerverwaltung',
+    path: "benutzerverwaltung",
     loadChildren: () =>
-      import('./pages/personen/personen.module').then(
+      import("./pages/personen/personen.module").then(
         (m) => m.PersonenPageModule
       ),
   },
   {
-    path: 'posteingang',
+    path: "posteingang",
     loadChildren: () =>
-      import('./pages/posteingang/posteingang.module').then(
+      import("./pages/posteingang/posteingang.module").then(
         (m) => m.PosteingangPageModule
       ),
   },
   {
-    path: 'kalender',
+    path: "kalender",
     loadChildren: () =>
-      import('./pages/kalender/kalender.module').then(
+      import("./pages/kalender/kalender.module").then(
         (m) => m.KalenderPageModule
       ),
   },
   {
-    path: 'krankmeldung',
+    path: "krankmeldung",
     loadChildren: () =>
-      import('./pages/krankmeldung/krankmeldung.module').then(
+      import("./pages/krankmeldung/krankmeldung.module").then(
         (m) => m.KrankmeldungPageModule
       ),
   },
   {
-    path: 'schuelermonitor',
+    path: "schuelermonitor",
     loadChildren: () =>
-      import('./pages/schuelermonitor/schuelermonitor.module').then(
+      import("./pages/schuelermonitor/schuelermonitor.module").then(
         (m) => m.SchuelermonitorPageModule
       ),
   },
   {
-    path: 'arbeitsgemeinschaften',
+    path: "arbeitsgemeinschaften",
     loadChildren: () =>
-      import('./pages/arbeitsgemeinschaften/arbeitsgemeinschaften.module').then(
+      import("./pages/arbeitsgemeinschaften/arbeitsgemeinschaften.module").then(
         (m) => m.ArbeitsgemeinschaftenPageModule
       ),
   },
   {
-    path: 'login',
+    path: "login",
     loadChildren: () =>
-      import('./pages/login/login.module').then((m) => m.LoginPageModule),
+      import("./pages/login/login.module").then((m) => m.LoginPageModule),
   },
 ];
 
